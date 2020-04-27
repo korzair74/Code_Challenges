66e3507f7bb08d8baad5d08bd12f7ba682209958
Chris Nickel <chris.nickel.dev@gmail.com>
2020-04-27 16:33:17 -0400

    update using reduce

--- Python/average.py
+++ Python/average.py
@@ -1,8 +1,15 @@
+# def average(num_list):
+#   mean = sum(num_list) / len(num_list)
+#   return mean
+
+# print(average([1, 2, 5, 75, 12, 43]))
 
 
+from functools import reduce
 
 def average(num_list):
-  mean = sum(num_list) / len(num_list)
-  return mean
-
+  total = reduce(lambda a,b : a+b,num_list) / len(num_list)
+  return total
+  
 print(average([1, 2, 5, 75, 12, 43]))
+
