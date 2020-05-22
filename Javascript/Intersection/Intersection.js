54ab685ab001ce4ff35aac61fbc635edf369e147
Chris Nickel <chris.nickel.dev@gmail.com>
2020-05-22 12:05:24 -0400

    Intersection Challenge correction

--- Javascript/Intersection/Intersection.js
+++ Javascript/Intersection/Intersection.js
@@ -1,11 +1,14 @@
 strArr = ['1, 3, 4, 7, 13', '1, 2, 4, 13, 15'];
 
 function FindIntersection(strArr) {
-	const arrA = strArr[0].split(',');
-	const arrB = strArr[1].split(',');
+	const arrA = strArr[0].split(', ');
+	const arrB = strArr[1].split(', ');
 	const intersection = arrA.filter((x) => arrB.includes(x));
-	// code goes here
-	return intersection;
+	if (intersection.length > 0) {
+		return intersection.join();
+	} else {
+		return false;
+	}
 }
 
 constole.log(FindIntersection(strArr));
